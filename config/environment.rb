bfe7a5d72f03e5caa8cc4342a26d18034c802659
Jose Luis Duran <jlduran@gmail.com>
2009-11-28 14:20:53 -0500

    Updated prawn.

--- config/environment.rb
+++ config/environment.rb
@@ -19,6 +19,7 @@ Rails::Initializer.run do |config|
   # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
+  config.gem "prawn"
   config.gem "authlogic"
   config.gem "mislav-will_paginate", :lib => "will_paginate", :source => "http://gems.github.com"
     
